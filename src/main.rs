d254a0b9ad3705a13079fce052167c553390b121
n1haldev <nihaltmdev@gmail.com>
2023-10-14 02:54:57 +0530

    Added some error handling to give status of clone

--- src/main.rs
+++ src/main.rs
@@ -24,17 +24,6 @@
  fn main() {
      let args = Args::parse();
  
-     // HashMap to store the entered args
-     // let mut info_map: HashMap<&String, Vec<Option<String>>> = HashMap::new();
-     // let mut info_vec: Vec<Option<String>> = Vec::new();
- 
-     // // Update the info vector with author, host and checkout info
-     // info_vec.push(args.host);
-     // info_vec.push(args.checkout);
- 
-     // // Insert repo name as key with info vector as value
-     // info_map.insert(&args.repo, info_vec);
- 
      // Git uname fetch
      let mut git_uname = Command::new("git");
  
@@ -47,16 +36,13 @@
      let mut stdout = String::from_utf8(git_uname.stdout).unwrap();
  
      stdout.pop();
- 
- 
+
      // Git pull your repo
      let host = args.host.unwrap_or("github".to_string());
      let checkout = args.checkout.unwrap_or("main".to_string());
-     let git_ssh_com = format!("git@{}.com:", host).to_string();
-     // let git_ssh_com: String = "git@github.com:".to_string();
      let repo: String = args.repo.to_string();
- 
-     // let final_com = format!("{}{}/{}.git#{}", git_ssh_com, stdout, repo, checkout);
+     let git_ssh_com = format!("git@{}.com:", host).to_string();
+
      let final_com = format!("{}{}/{}.git", git_ssh_com, stdout, repo);
      println!("{}", final_com);
  
@@ -64,5 +50,21 @@
  
      gegit_com.arg("clone").arg("-b").arg(checkout).arg(final_com);
      gegit_com.output().unwrap();
+
+     // Checking if the repo was cloned successfully
+    let (command, arg) = if cfg!(target_os = "windows") {
+        ("dir", "/B")
+    } else {
+        ("ls", "")
+    };
+
+    let op = Command::new(command).arg(arg).output().unwrap();
+
+    if op.status.success() {
+        println!("Repo {} cloned successfully", repo);
+    } else {
+        println!("Check if repo name and branch name are correct");
+    }
+
  }
  
